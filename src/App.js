c38c96105a1042ed6018ae45d9a502df1598420d
Samer <samerbenmim@insat.u-carthage.tn>
2022-06-07 19:56:16 +0100

    added auth

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-    
-          Hi Samer
-        
+      <header>
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>We now have Auth!</h1>
       </header>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import Amplify from 'aws-amplify';
+import config from './aws-exports';
+Amplify.configure(config);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
